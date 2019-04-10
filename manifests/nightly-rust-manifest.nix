fb8ba1ad8a4c09f179e0277075bb9b27a29bbd06
Brett Gilio <brettg@posteo.net>
2019-04-09 23:00:37 -0500

    Update manifests/nightly-rust-manifest.nix: Add src and analysis

--- manifests/nightly-rust-manifest.nix
+++ manifests/nightly-rust-manifest.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     buildInputs = [ pkgs.rustChannels.nightly.rust
-    		    pkgs.rls];
+    		    pkgs.rustChannels.nightly.rust-src
+		    pkgs.rustChannels.nightly.rust-analysis
+    		    pkgs.rls ];
 }

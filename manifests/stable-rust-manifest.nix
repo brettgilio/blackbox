18e5a6601c2c1f75038b8f1e262649ab30379e43
Brett Gilio <brettg@posteo.net>
2019-04-10 00:36:22 -0500

    Update manifests/stable-rust-manifest.nix: Format and shell-hook

--- manifests/stable-rust-manifest.nix
+++ manifests/stable-rust-manifest.nix
@@ -1,7 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs = [ pkgs.rustChannels.stable.rust
-    		    pkgs.rustChannels.stable.rust-src
-		    pkgs.rustChannels.stable.rust-analysis
-    		    pkgs.rls ];
+with import <nixpkgs> {};
+
+pkgs.mkShell {
+  buildInputs = [ pkgs.rustChannels.stable.rust
+    		  pkgs.rustChannels.stable.rust-src
+		  pkgs.rustChannels.stable.rust-analysis
+    		  pkgs.rls ];
+
+
+  shellHook = ''
+    	      export RUST_SRC_PATH="${pkgs.rustChannels.stable.rust-src}/lib/rustlib/src/rust/src"
+  	      '';
 }

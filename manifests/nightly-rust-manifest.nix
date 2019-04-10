71ea28d2ba5b3f8f9b3f098fcb613decac6865db
Brett Gilio <brettg@posteo.net>
2019-04-10 00:36:05 -0500

    Update manifests/nightly-rust-manifest.nix: Format and shell-hook

--- manifests/nightly-rust-manifest.nix
+++ manifests/nightly-rust-manifest.nix
@@ -1,7 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs = [ pkgs.rustChannels.nightly.rust
-    		    pkgs.rustChannels.nightly.rust-src
-		    pkgs.rustChannels.nightly.rust-analysis
-    		    pkgs.rls ];
+with import <nixpkgs> {};
+
+pkgs.mkShell {
+  buildInputs = [ pkgs.rustChannels.nightly.rust
+    		  pkgs.rustChannels.nightly.rust-src
+		  pkgs.rustChannels.nightly.rust-analysis
+    		  pkgs.rls ];
+
+
+  shellHook = ''
+    	      export RUST_SRC_PATH="${pkgs.rustChannels.nightly.rust-src}/lib/rustlib/src/rust/src"
+  	      '';
 }

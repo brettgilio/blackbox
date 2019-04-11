0366a70faf19e04a89076b97bab84feadbcbed75
Brett Gilio <brettg@posteo.net>
2019-04-11 13:50:21 -0500

    Update .emacs.d/modules/module-pg.el: Wrap in defun

--- .emacs.d/modules/module-pg.el
+++ .emacs.d/modules/module-pg.el
@@ -1,6 +1,7 @@
+(defun module-pg//load-pg ()
 (load (concat
        (getenv "GUIX_ENVIRONMENT")
        "/share/emacs/site-lisp"
-       "/ProofGeneral/generic/proof-site.el"))
+       "/ProofGeneral/generic/proof-site.el")))
 
 (provide 'module-pg)

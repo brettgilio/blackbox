3e398e8e00209efe2a9379bb12c473d492db928d
Brett Gilio <brettg@posteo.net>
2019-04-11 12:40:00 -0500

    Update .emacs.d/modules/module-appearance.el: Create primary func...

--- .emacs.d/modules/module-appearance.el
+++ .emacs.d/modules/module-appearance.el
@@ -1,4 +1,25 @@
-(provide 'module-appearance)
+(defun module-appearance//default-theme (&optional _frame)
+  (set-face-attribute 'default nil
+		      :family "Source Code Pro"
+		      :height 140
+		      :weight 'regular)
+  
+  (display-time-mode)
+  (global-hl-line-mode 1)
+  
+  (defun module-appearance//default-theme//doom-modeline-daemon (_frame)
+    "`doom-modeline' strangely does not handle well under a daemonized Emacs."
+    (setq doom-modeline-icon t)
+    (setq doom-modeline-height 35))
+  
+  (use-package doom-modeline
+    :hook ((after-init . doom-modeline-mode)
+	   (after-make-frame-functions . module-appearance//default-theme//doom-modeline-daemon)))
+  
+  (use-package display-line-numbers
+    :hook (prog-mode . display-line-numbers-mode)))
 
-(use-package doom-modeline
-  :hook (after-init . doom-modeline-mode))
+(module-appearance//default-theme)
+
+
+(provide 'module-appearance)

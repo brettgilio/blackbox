86e2a1a57b8de60dfd132e3e4739f3e3bed41828
Brett Gilio <brettg@posteo.net>
2019-04-12 16:19:27 -0500

    Update .emacs.d/modules/module-pg.el: Add ext hook, and company-coq

--- .emacs.d/modules/module-pg.el
+++ .emacs.d/modules/module-pg.el
@@ -5,7 +5,12 @@
 		 (expand-file-name
 		  "share/emacs/site-lisp/ProofGeneral/generic"
 		  (or (getenv "GUIX_ENVIRONMENT")
-		      (error "GUIX_ENVIRONMENT is not defined."))))))
+		      (error "GUIX_ENVIRONMENT is not defined."))))
+    :mode ("\\.v\\'" . coq-mode))
+
+  (use-package company-coq
+    :after (proof-site)
+    :hook (coq-mode . company-coq-mode)))
   
 
 (provide 'module-pg)

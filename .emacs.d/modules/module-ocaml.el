208f34bf83a7d69e60fe9c60d23550dca2ef8fdf
Brett Gilio <brettg@posteo.net>
2019-04-15 23:50:13 -0500

    Add .emacs.d/modules/module-ocaml.el: Partial outline

---
+++ .emacs.d/modules/module-ocaml.el
@@ -0,0 +1,19 @@
+(defun module-ocaml//load-merlin ()
+
+  (use-package merlin
+    :config (setq merlin-command
+		  (lambda ()
+		    (expand-file-name
+		     "bin/ocamlmerlin"
+		     (or (getenv "GUIX_ENVIRONMENT")
+			 (error "GUIX_ENVIRONMENT is not defined."))))))
+
+  (use-package tuareg
+    :after (merlin)
+    :mode ("\\.ml\\'" . tuareg-mode)
+    :hook ((tuareg-mode . merlin-mode))))
+
+;; Implement merlin-company support
+;; https://github.com/ocaml/merlin/blob/master/emacs/merlin-company.el
+
+(provide 'module-ocaml)

39f7038efe5e11616c066d07d51e875b8786ed64
Brett Gilio <brettg@posteo.net>
2019-04-17 14:23:28 -0500

    Update bin/configuration-install.sh: Fix directory check

--- bin/configuration-install.sh
+++ bin/configuration-install.sh
@@ -21,9 +21,9 @@ done
 
 for configs in emacs.d stumpwm.d
 
-do # This is failing to recognize that the directory exists. Fix.
-    if [ -f "$HOME/.${configs}" ]; then
-	echo "$HOME/.{configs} exists, skipping..."
+do
+    if [ -d "$HOME/.${configs}" ]; then
+	echo "$HOME/.${configs} exists, skipping..."
     else
 	echo "Creating $HOME/.${configs}..."
 	ln -rs ".${configs}" "$HOME" 

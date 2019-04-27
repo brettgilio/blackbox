762809ac1bcdc20bc97f1ddfe9aa9bf04749693a
Brett Gilio <brettg@posteo.net>
2019-04-26 21:36:00 -0500

    Rename manifests/{mono-fsh-manifest.nix => dotnet-fsh-manifest.nix}

--- manifests/dotnet-fsh-manifest.nix
+++ manifests/dotnet-fsh-manifest.nix
@@ -1,12 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
 (pkgs.buildFHSUserEnv {
-  name = "mono-fsh-manifest";
+  name = "dotnet-fsh-manifest";
   targetPkgs = pkgs: (with pkgs;
     [ udev
       alsaLib
+      dotnet-sdk
       mono
       gmp
+      wget
     ]) ++ (with pkgs.xorg;
     [ libX11
       libXcursor

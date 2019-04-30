95461ed8013b9ae0cecc34164a02ca0c8f6c1a2b
Brett Gilio <brettg@posteo.net>
2019-04-30 14:40:19 -0500

    Update manifests/nightly-rust-manifest.nix: Declare let-in

--- manifests/nightly-rust-manifest.nix
+++ manifests/nightly-rust-manifest.nix
@@ -1,13 +1,24 @@
+# Thank you @Laalf
+
 with import <nixpkgs> {};
 
-pkgs.mkShell {
-  buildInputs = [ pkgs.rustChannels.nightly.rust
-    		  pkgs.rustChannels.nightly.rust-src
-		  pkgs.rustChannels.nightly.rust-analysis
-    		  pkgs.rls ];
+let
+  mozilla-overlay = import (builtins.fetchGit { url = https://github.com/mozilla/nixpkgs-mozilla.git; ref = "master";});
+  nixpkgs-master = import (builtins.fetchGit { url = https://github.com/NixOS/nixpkgs.git; ref = "master";})  { overlays = [ mozilla-overlay ]; };
+  
+in
+  with nixpkgs-master;
+  stdenv.mkDerivation {
+      name = "nix_is_painful";
+      buildInputs = [
+          pkgs.rustChannels.nightly.rust
+          pkgs.rustChannels.nightly.rust-src
+          pkgs.rustChannels.nightly.rust-analysis
+          pkgs.rls
+    ];
 
 
   shellHook = ''
-    	      export RUST_SRC_PATH="${pkgs.rustChannels.nightly.rust-src}/lib/rustlib/src/rust/src"
-  	      '';
+              export RUST_SRC_PATH="${pkgs.rustChannels.nightly.rust-src}/lib/rustlib/src/rust/src"
+              '';
 }

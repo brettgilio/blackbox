47d18e61bce13eabf1ce3ed4d5abd9033ed4d928
Brett Gilio <brettg@posteo.net>
2019-05-08 15:36:36 -0500

    Update manifests/guix/ocaml-manifest.scm: Add opam packages

--- manifests/guix/ocaml-manifest.scm
+++ manifests/guix/ocaml-manifest.scm
@@ -5,7 +5,13 @@
    "ocaml-findlib"
    "ocaml-merlin"
    "ocaml-utop"
-   "opam"))
+   "opam"
+   "rsync"
+   "mercurial"
+   "darcs"
+   "make"
+   "gcc-toolchain"
+   "m4"))
 
 ;; Pair "ocaml-manifest.nix" is for LSP. The LSP fails to start
 ;; so Merlin will be used for completion in the meantime.

102ac0510804b4c11246d9f6d5e94dd4dcd829b0
Brett Gilio <brettg@posteo.net>
2019-05-16 20:40:10 -0500

    Update manifests/guix/fstar-manifest.scm: Bump fstar-mode

--- manifests/guix/fstar-manifest.scm
+++ manifests/guix/fstar-manifest.scm
@@ -1,6 +1,6 @@
 ;; To be used in combination with a Nix file associated with Fstar
 
 (specifications->manifest
- '("emacs-fstar-mode@0.9.4.0-1.d63d1c4"
+ '("emacs-fstar-mode@0.9.4.0-2.b2540d2"
    "gdb"
    "z3"))

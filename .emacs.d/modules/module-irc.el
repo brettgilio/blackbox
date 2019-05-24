6df1e4d663a6b14358110858e95ce02a261ea374
Brett Gilio <brettg@posteo.net>
2019-05-24 16:17:29 -0500

    Add .emacs.d/modules/module-irc.el

---
+++ .emacs.d/modules/module-irc.el
@@ -0,0 +1,23 @@
+(defun module-irc//load-erc ()
+  (interactive)
+
+  (require 'erc-services)
+  (erc-services-mode 1)
+
+  (setq erc-autojoin-channels-alist ; fails to join all channels
+	'(("freenode.net"
+	   "#emacs"
+	   "##dependent"
+	   "#scheme"
+	   "#guile"
+	   "#guix"
+	   "#idris"
+	   "#coq")))
+
+  (setq erc-prompt-for-nickserv-password t)
+
+  (erc :server "irc.freenode.net"
+       :port 6667
+       :nick "brettgilio"))
+
+(provide 'module-irc)

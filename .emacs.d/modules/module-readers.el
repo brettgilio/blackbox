98e07594b3787475e7ca1123c0caa2c36c5ab9ee
Brett Gilio <brettg@posteo.net>
2019-05-24 00:24:46 -0500

    Update .emacs.d/modules/module-readers.el: Add webpaste config

--- .emacs.d/modules/module-readers.el
+++ .emacs.d/modules/module-readers.el
@@ -2,6 +2,11 @@
 
   (use-package pdf-tools
     :magic ("%PDF" . pdf-view-mode)
-    :config (pdf-tools-install :no-query)))
+    :config (pdf-tools-install :no-query))
+
+  (use-package webpaste
+    :config (setq webpaste-provider-priority '("ix.io"))
+    :bind (("C-c C-p C-b" . webpaste-paste-buffer)
+	   ("C-c C-p C-r" . webpaste-paste-region))))
 
 (provide 'module-readers)

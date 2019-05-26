395471530844abf0adaccde23eeb129a789bd60f
Brett Gilio <brettg@posteo.net>
2019-05-25 19:29:43 -0500

    Update .emacs.d/modules/module-irc.el: Fix identifying and joinin

--- .emacs.d/modules/module-irc.el
+++ .emacs.d/modules/module-irc.el
@@ -1,23 +1,24 @@
 (defun module-irc//load-erc ()
   (interactive)
-
+  
   (require 'erc-services)
   (erc-services-mode 1)
-
+  
   (setq erc-prompt-for-nickserv-password t)
-
+  (setq erc-autojoin-timing 'ident)
+  
   (erc :server "irc.freenode.net"
        :port 6667
        :nick "brettgilio")
 
-  (setq erc-autojoin-channels-alist ; fails to join all channels
-	'(("freenode.net"
-	   "#emacs"
-	   "##dependent"
-	   "#scheme"
-	   "#guile"
-	   "#guix"
-	   "#idris"
+  (setq erc-autojoin-channels-alist
+  	'(("freenode.net"
+  	   "#emacs"
+  	   "##dependent"
+  	   "#scheme"
+  	   "#guile"
+  	   "#guix"
+  	   "#idris"
 	   "#coq"))))
-  
+
 (provide 'module-irc)

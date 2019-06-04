0fefcab00dab5330ebac2e4129a339b81db999cb
Brett Gilio <brettg@posteo.net>
2019-06-03 21:30:26 -0500

    Delete manifests/guix/rust-manifest.scm

--- manifests/guix/rust-manifest.scm
+++
@@ -1,7 +0,0 @@
-;; To be used in combination with a Nix file associated with Rust
-
-(specifications->manifest
- '("emacs-company-lsp"
-   "emacs-lsp-mode"
-   "emacs-lsp-ui"
-   "emacs-rust-mode"))

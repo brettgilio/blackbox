774dced83ff2566468ffa387fe9af6d3a2fb4332
Brett Gilio <brettg@posteo.net>
2019-06-06 21:52:11 -0500

    Update .emacs.d/modules/module-readers.el: Add nov

--- .emacs.d/modules/module-readers.el
+++ .emacs.d/modules/module-readers.el
@@ -7,6 +7,11 @@
   (use-package webpaste
     :config (setq webpaste-provider-priority '("ix.io"))
     :bind (("C-c C-p C-b" . webpaste-paste-buffer)
-	   ("C-c C-p C-r" . webpaste-paste-region))))
+	   ("C-c C-p C-r" . webpaste-paste-region)))
+
+  ;; Make into USE-PACKAGE
+  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
+
+
 
 (provide 'module-readers)

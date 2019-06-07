df2496c41d1edb077a46975a7a2adb0d9fadf176
Brett Gilio <brettg@posteo.net>
2019-06-06 22:35:33 -0500

    Update channels.scm: Manually manage guix mainline commit ref

--- channels.scm
+++ channels.scm
@@ -1,7 +1,14 @@
 (define-module (channels)
   #:use-module (guix channels))
 
-(cons (channel
+(list (channel
        (name 'guix-channel)
        (url "https://git.sr.ht/~brettgilio/guix-channel"))
-      %default-channels)
+      (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit
+	"8190e1edafac77606026f414a5951bde759bd26e")))
+
+;; Manually manage mainline guix commits
+;; LOG: http://git.savannah.gnu.org/cgit/guix.git/log/

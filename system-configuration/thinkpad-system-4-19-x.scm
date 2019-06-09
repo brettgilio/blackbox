a6f586251bb3ecb5abba7f158f78cb795d741b36
Brett Gilio <brettg@posteo.net>
2019-06-08 21:22:21 -0500

    Update system-configuration/thinkpad-system-4-19-x.scm: Move kernel

--- system-configuration/thinkpad-system-4-19-x.scm
+++ system-configuration/thinkpad-system-4-19-x.scm
@@ -18,60 +18,7 @@
   #:use-module (srfi srfi-1))
 
 (use-service-modules desktop xorg)
-(use-package-modules certs gnome)(define (linux-nonfree-urls version)
-  "Return a list of URLs for Linux-Nonfree VERSION."
-  (list (string-append
-         "https://www.kernel.org/pub/linux/kernel/v4.x/"
-         "linux-" version ".tar.xz")))
-
-(define-public linux-nonfree-4.19
-  (let* ((version "4.19.48"))
-    (package
-     (inherit linux-libre)
-     (name "linux-nonfree")
-     (version version)
-     (source (origin
-	      (method url-fetch)
-	      (uri (linux-nonfree-urls version))
-	      (sha256
-	       (base32
-		"1ylkd30hg2skfxl12g45lqm48b732faf41d68513i3nc217xpl01"))))
-     (synopsis "Mainline Linux kernel, nonfree binary blobs included.")
-     (description "Linux is a kernel.")
-     (home-page "http://kernel.org/")
-     (license license:gpl2))))
-
-(define-public iwlwifi-firmware-nonfree
-  (package
-   (name "iwlwifi-firmware-nonfree")
-   (version "711d3297bac870af42088a467459a0634c1970ca")
-   (source (origin
-	    (method git-fetch)
-	    (uri (git-reference
-		  (url "git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git")
-		  (commit version)))
-	    (sha256
-	     (base32
-	      "0dbnib2gh8kajyys3pps99lp5s4k6b26v2jmbq2x3vbl9fa1xgda"))))
-   (build-system trivial-build-system)
-   (arguments
-    `(#:modules ((guix build utils))
-      #:builder (begin
-		  (use-modules (guix build utils))
-		  (let ((source (assoc-ref %build-inputs "source"))
-			(fw-dir (string-append %output "/lib/firmware")))
-		    (mkdir-p fw-dir)
-		    (for-each (lambda (file)
-				(copy-file file
-					   (string-append fw-dir "/"
-							  (basename file))))
-			      (find-files source "iwlwifi-.*\\.ucode$|LICENCE\\.iwlwifi_firmware$"))
-		    #t))))
-   (home-page "https://wireless.wiki.kernel.org/en/users/drivers/iwlwifi")
-   (synopsis "Non-free firmware for Intel wifi chips")
-   (description "Non-free firmware for Intel wifi chips")
-   (license (license:non-copyleft "http://git.kernel.org/?p=linux/kernel/git/firmware/linux-firmware.git;a=blob_plain;f=LICENCE.iwlwifi_firmware;hb=HEAD"))))
-
+(use-package-modules certs gnome)
 
 (operating-system
  (host-name "thinkpad")

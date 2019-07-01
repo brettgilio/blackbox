23f2333b05d35eca680b509cb181e0139fd9735e
Brett Gilio <brettg@posteo.net>
2019-06-30 22:35:52 -0500

    Update .emacs.d/modules/module-lilypond.el: defer loading

--- .emacs.d/modules/module-lilypond.el
+++ .emacs.d/modules/module-lilypond.el
@@ -10,7 +10,8 @@
     (autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
     (add-to-list 'auto-mode-alist '("\\.ly$" . LilyPond-mode))
     (add-to-list 'auto-mode-alist '("\\.ily$" . LilyPond-mode))
-    (add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))))
+    (add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
+    :defer t))
 
 
 (provide 'module-lilypond)

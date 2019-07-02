466bea7b986682f3d2e5a6c778eafd353e5bf90b
Brett Gilio <brettg@posteo.net>
2019-07-01 19:16:14 -0500

    Update channels.scm: Bump guix commit

--- channels.scm
+++ channels.scm
@@ -8,7 +8,7 @@
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
        (commit
-	"87a90486ca63939d0ef73bdc8e9a26fef03afccb")))
+	"cea5db1b8454c02452bed8cbe6eb89ce37164fe8")))
 
 ;; Manually manage mainline guix commits
 ;; LOG: http://git.savannah.gnu.org/cgit/guix.git/log/

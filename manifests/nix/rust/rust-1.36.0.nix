cc118b98ffaa91f8476fd71451a7ded34c3d51ed
Brett Gilio <brettg@posteo.net>
2019-07-08 17:13:48 -0500

    Add manifests/nix/rust/rust-1.36.0.nix

---
+++ manifests/nix/rust/rust-1.36.0.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+
+let
+  mozilla-overlay = import (builtins.fetchGit { url = https://github.com/mozilla/nixpkgs-mozilla.git; ref = "master";});
+  nixpkgs-master = import (builtins.fetchGit { url = https://github.com/NixOS/nixpkgs.git; ref = "master";})  { overlays = [ mozilla-overlay ]; };
+  
+  rustStable = nixpkgs-master.rustChannelOf {
+    channel = "1.36.0";
+  };
+  
+in
+with nixpkgs-master;
+stdenv.mkDerivation {
+  name = "rust-stable";
+  buildInputs = [
+    rustStable.rust
+  ];
+  
+  
+  shellHook = ''
+              export RUST_SRC_PATH="${rustStable.rust-src}/lib/rustlib/src/rust/src"
+              '';
+}

a50a4cdf48b1fe3ac6138f5697064ebb87870180
Brett Gilio <brettg@posteo.net>
2019-07-25 13:13:17 -0500

    Update manifests/nix/haskell-manifest.nix: Fix file formatting

--- manifests/nix/haskell-manifest.nix
+++ manifests/nix/haskell-manifest.nix
@@ -6,32 +6,29 @@ with pkgs;
 
 let
   all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-
+  
   hs = [ cabal-install
          ghc
          gmp
          stack
          haskellPackages.alex
          haskellPackages.happy ];
+  
 in
-  mkShell {
-    name = "${compiler}-sh";
-
-    buildInputs = [ hs
-                    (all-hies.selection { selector = p: { inherit (p)
-                      ghc865
-                      ghc864
-                      ghc863
-                      ghc862
-                      ghc861
-                      ghc844
-                      ghc843
-                      ghc842
-                      ghc822; }; })
-                  ];
-    
-    # shellHook = ''
-    #  eval "$(egrep ^export "$(type -p ghc)")"
-    #  export PS1="\[\033[1;32m\][$name:\W]\n$ \[\033[0m\]"
-    # '';
+mkShell {
+  name = "${compiler}-sh";
+  
+  buildInputs = [ hs
+                  (all-hies.selection { selector = p: { inherit (p)
+                    ghc865
+                    ghc864
+                    ghc863
+                    ghc862
+                    ghc861
+                    ghc844
+                    ghc843
+                    ghc842
+                    ghc822; }; })
+                ];
+  
 }

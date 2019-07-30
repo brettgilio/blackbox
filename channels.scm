e4bc4cac06cf9ad542ee09b1c9a4ca90c38e39ab
Brett Gilio <brettg@posteo.net>
2019-07-29 21:37:06 -0500

    Update channels.scm: Add guix-home-manager

--- channels.scm
+++ channels.scm
@@ -8,7 +8,13 @@
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
        (commit
-	"76b96720e20f109138c7fca027c9f0deb2b40d26")))
+	"76b96720e20f109138c7fca027c9f0deb2b40d26"))
+      (channel
+       (name 'guix-hm)
+       (url "https://framagit.org/tyreunom/guix-home-manager.git")
+       (commit
+	"35e30a169c2328657a4d402b3c1ad5ac0efff3d2")))
 
 ;; Manually manage mainline guix commits
 ;; LOG: http://git.savannah.gnu.org/cgit/guix.git/log/
+;; LOG: https://framagit.org/tyreunom/guix-home-manager/

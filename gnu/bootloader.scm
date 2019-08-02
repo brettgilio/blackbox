8c7f5fbbded6781666e5d5065869f30a6858314b
Brett Gilio <brettg@posteo.net>
2019-08-02 15:20:09 -0500

    Add gnu/bootloader.scm

---
+++ gnu/bootloader.scm
@@ -0,0 +1,26 @@
+;;; This file is part of Blackbox, a fork of GNU Guix.
+;;;
+;;; Original Guix Authors:
+;;; Copyright © 2017 David Craven <david@craven.ch>
+;;; Copyright © 2017 Mathieu Othacehe <m.othacehe@gmail.com>
+;;; Copyright © 2017 Leo Famulari <leo@famulari.name>
+;;;
+;;; Blackbox Authors:
+;;; Copyright © 2019 Brett Gilio <brettg@posteo.net>
+;;;
+;;; Blackbox is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; Blackbox is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Blackbox.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu bootloader))
+
+;;; bootloader.scm ends here

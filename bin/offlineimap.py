e3a994af354957a777b9b23f8d445b800c8fb998
Brett Gilio <brettg@posteo.net>
2019-08-02 14:17:09 -0500

    Delete bin/offlineimap.py

--- bin/offlineimap.py
+++
@@ -1,7 +0,0 @@
-#!/run/current-system/profile/bin/python3
-
-from subprocess import check_output
-
-def get_pass(account):
-    raw_output = check_output("pass Mail/" + account, shell=True).splitlines()[0]
-    return raw_output
